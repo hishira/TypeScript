902cd82df2e4a8bf06ff8880b02616c372e6fbf9
hishira <okami0124@gmail.com>
2021-09-12 18:25:47 +0200

    Uptade photo component

--- PetShelter/frontend/src/app/pet-photos/pet-photos.component.ts
+++ PetShelter/frontend/src/app/pet-photos/pet-photos.component.ts
@@ -1,21 +1,35 @@
-import { Component, OnInit,Input } from '@angular/core';
-import {PhotoService} from "../photo.service";
-import {Photo} from "../photo";
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import { PhotoService } from '../photo.service';
+import { Photo } from '../photo';
 @Component({
   selector: 'app-pet-photos',
   templateUrl: './pet-photos.component.html',
-  styleUrls: ['./pet-photos.component.css']
+  styleUrls: ['./pet-photos.component.css'],
 })
 export class PetPhotosComponent implements OnInit {
   @Input() petid?: number;
-  photos?: Array<Photo>
-  constructor(private photoservice: PhotoService) { }
+  @ViewChild('imgelement', { static: false }) imageelement?: ElementRef;
+
+  photos?: Array<Photo>;
+  photoindex: number = 0;
+  constructor(private photoservice: PhotoService) {}
 
   ngOnInit(): void {
-    if(this.petid)
+    if (this.petid) {
       this.photoservice
-          .getPetPhotos(this.petid)
-          .subscribe(photos=>this.photos = photos)
+        .getPetPhotos(this.petid)
+        .subscribe((photos) => (this.photos = photos));
+      this.selectPhoto();
+    }
   }
 
+  selectPhoto(): void {
+    setInterval(() => {
+      console.log(this.photos)
+      if (this.imageelement && this.photos) {
+        this.photoindex =
+          this.photoindex + 1 >= this.photos.length ? 0 : this.photoindex + 1;
+      }
+    }, 2000);
+  }
 }
