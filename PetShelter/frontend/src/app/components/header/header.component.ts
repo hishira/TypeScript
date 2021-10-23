cca82b8bf6aa904029bf34fdb203a18793a4d521
hishira <okami0124@gmail.com>
2021-10-23 20:54:29 +0200

    Chande header, make hamburger

--- PetShelter/frontend/src/app/components/header/header.component.ts
+++ PetShelter/frontend/src/app/components/header/header.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  private hamburgerElement?: HTMLElement;
+  private nestednav?: HTMLElement;
+  private readonly HAMBURGEROPEN: string = 'hamburger-open'
   constructor() { }
 
   ngOnInit(): void {
+    this.hamburgerElement = document.getElementsByClassName('hamburger')[0] as HTMLElement
+    this.nestednav = document.getElementsByClassName('nasted')[0] as HTMLElement;
   }
 
+  hamburgerClick(): void {
+    this.hamburgerElement?.classList.toggle(this.HAMBURGEROPEN);
+    this.nestednav?.classList.toggle('nested-open')
+  }
+
+  resetCSSClasses(): void {
+    if(this.nestednav?.classList.contains('nested-open')){
+      this.nestednav?.classList.remove('nested-open')
+    }
+    if(this.hamburgerElement?.classList.contains(this.HAMBURGEROPEN)){
+      this.hamburgerElement?.classList.remove(this.HAMBURGEROPEN)
+    }
+
+  }
 }
