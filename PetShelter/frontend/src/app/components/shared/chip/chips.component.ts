721b6fb5ddbabbfd0a802e61adf362bcbecebceb
hishira <okami0124@gmail.com>
2022-04-27 23:40:02 +0200

    add close icon to chip

--- PetShelter/frontend/src/app/components/shared/chip/chips.component.ts
+++ PetShelter/frontend/src/app/components/shared/chip/chips.component.ts
@@ -8,6 +8,7 @@ import { Component, Input } from "@angular/core";
 export class ChipComponent {
 
     @Input() label: string;
+    @Input() closeHandle? : boolean = false
     constructor(){
     }
 }
