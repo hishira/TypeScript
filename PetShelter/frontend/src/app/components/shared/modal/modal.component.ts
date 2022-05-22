d82a773c89dd3219594e391a6ab59faf81b2b820
hishira <okami0124@gmail.com>
2022-05-22 18:07:29 +0200

    small improvements af frontend side

--- PetShelter/frontend/src/app/components/shared/modal/modal.component.ts
+++ PetShelter/frontend/src/app/components/shared/modal/modal.component.ts
@@ -1,5 +1,4 @@
 import { Component, Injector, OnInit, Type } from '@angular/core';
-import { Subject } from 'rxjs';
 import { ModalProps } from 'src/app/models/modal-props.model';
 import { ModalService } from 'src/app/services/modal.service';
 import { DocumentService } from 'src/app/services/document.service';
@@ -22,14 +21,14 @@ export class ModalComponent implements OnInit {
       .getContent()
       .subscribe((value: { component: Type<Component>; props: ModalProps }) => {
         this.modalContent = value.component;
-        //console.log(value.props);
+
       });
     this.modalService.checkDialogOpen().subscribe((value: boolean) => {
       this.open = value;
       if(this.open === true) {
-        this.documentService.changeTagCssProperty('body', 'overflow', 'hidden');
+        this.documentService.changeTagCssProperty('body', 'overflow-y', 'scroll');
       } else {
-        this.documentService.changeTagCssProperty('body', 'overflow', 'auto');
+        this.documentService.changeTagCssProperty('body', 'overflow-y', 'auto');
       }
     });
    
--- PetShelter/frontend/src/app/services/document.service.ts
+++ PetShelter/frontend/src/app/services/document.service.ts
@@ -4,7 +4,9 @@ import { Injectable } from "@angular/core";
     providedIn: 'root',
 })
 export class DocumentService {
+
     constructor(){}
+
     changeTagCssProperty(tagName: string, cssProperty: string, cssValue: string) {
         const documenttag = document.getElementsByTagName(tagName)[0] as HTMLElement;
         documenttag.setAttribute('style', `${cssProperty}:${cssValue}`);
