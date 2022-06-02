7aa79209025c5d4b499144e1381d05700f2fd8e7
hishira <okami0124@gmail.com>
2022-06-03 00:33:12 +0200

    eslint update, fix some eslint errors

--- PetShelter/frontend/src/app/components/shared/select-list/select-list.component.ts
+++ PetShelter/frontend/src/app/components/shared/select-list/select-list.component.ts
@@ -4,16 +4,15 @@ import { ModalService } from 'src/app/services/modal.service';
 import { SelectListModalComponent } from './select-list-modal/select-list-modal.component';
 @Component({
   selector: 'sh-select-list',
-  templateUrl: './select-list.component.html',
   styleUrls: ['./select-list.component.css'],
+  templateUrl: './select-list.component.html',
 })
 export class SelectListComponent implements OnInit {
   @Input() control: AbstractControl | null = null;
   @Input() key: string = '';
   @Input() placeholder: string = '';
-  @Input() selectList: Array<any> = [];
-  
   @Output() selectEmit: EventEmitter<any> = new EventEmitter<any>();
+  @Input() selectList: Array<any> = [];
   
   public down: boolean = true;
   public placeholderSave: string = '';
--- PetShelter/frontend/src/app/components/shelterpets/shelterpets.component.ts
+++ PetShelter/frontend/src/app/components/shelterpets/shelterpets.component.ts
@@ -1,84 +1,80 @@
-import { Component, OnInit, Input, OnChanges, SimpleChange, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 import { PetService } from '../../services/pet.service';
 import { Pet } from '../../models/pet.model';
-import { Router } from '@angular/router'; 
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-shelterpets',
   styleUrls: ['./shelterpets.component.css'],
   templateUrl: './shelterpets.component.html',
 })
 export class ShelterpetsComponent implements OnInit, OnChanges {
-  
   @Input() pets?: Array<Pet>;
-  public selectedPet: Pet | undefined;
   public petsShow: boolean = true;
-  private intervalCheck: NodeJS.Timer | null = null;
+  public selectedPet: Pet | undefined;
   private afterFilterInterval: NodeJS.Timer | null = null;
+  private intervalCheck: NodeJS.Timer | null = null;
   private petAppearInterval: number = 0;
-  constructor(private petService: PetService,
-      private router: Router) {}
 
- 
-  // TODO Fix problem with filter pet array
-  ngOnInit(): void {
-    this.pets = this.pets;
-    this.petAppearInterval = 0;
-    this.petsShow = true;
-    this.intervalStart();
-    console.log(this.pets)
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    if(!changes.pets.firstChange && changes.pets.currentValue !== []) {
-        this.pets = changes.pets.currentValue;
-        this.petAppearInterval = 0;
-        this.intervalCheck = null
-        this.brokenIntervalAfterFilter();
-    }
-
-  }
-  choosePet(pet: Pet): string {
-    //this.selectedPet = pet;
-    //this.petsShow = false;
-    return `${pet.id}`;
-  }
+  constructor(private petService: PetService, private router: Router) {}
 
   backPageHandle(): void {
     this.petsShow = true;
     this.petAppearInterval = 0;
     this.intervalStart();
   }
-  private conditionCheck(elements: HTMLCollectionOf<Element>): boolean {
-    return (
-      this.pets !== undefined &&
-      this.pets.length > 0 &&
-      elements.length === this.pets.length
-    );
+
+  choosePet(pet: Pet): string {
+    //this.selectedPet = pet;
+    //this.petsShow = false;
+    return `${pet.id}`;
   }
 
-  private setPetOpacity(pet: HTMLElement) {
-    pet.classList.add('appear')
+  ngOnChanges(changes: SimpleChanges): void {
+    if (!changes.pets.firstChange && changes.pets.currentValue !== []) {
+      this.pets = changes.pets.currentValue;
+      this.petAppearInterval = 0;
+      this.intervalCheck = null;
+      this.brokenIntervalAfterFilter();
+    }
+  }
+
+  // TODO Fix problem with filter pet array
+  ngOnInit(): void {
+    this.pets = this.pets;
+    this.petAppearInterval = 0;
+    this.petsShow = true;
+    this.intervalStart();
+    console.log(this.pets);
+  }
+
+  private appearPetElement(petelement: HTMLCollectionOf<HTMLElement>) {
+    Array.from(petelement).forEach((pet) => {
+      setTimeout(() => {
+        this.setPetOpacity(pet);
+      }, this.petAppearInterval);
+      this.petAppearInterval += 200;
+      console.log(this.petAppearInterval);
+    });
   }
 
   // TODO Check if we can fix it, after filter from empty to something get
   // into loop
   private brokenIntervalAfterFilter(): void {
-    this.afterFilterInterval = setInterval( ()=>{
+    this.afterFilterInterval = setInterval(() => {
       const elements: HTMLCollectionOf<Element> =
-      document.getElementsByClassName('pet');
-      console.log("Searching")
-      if(elements && this.afterFilterInterval){
+        document.getElementsByClassName('pet');
+      console.log('Searching');
+      if (elements && this.afterFilterInterval) {
         this.appearPetElement(elements as HTMLCollectionOf<HTMLElement>);
-        clearInterval(this.afterFilterInterval)
+        clearInterval(this.afterFilterInterval);
       }
-    },50)
-  }
-  private appearPetElement(petelement: HTMLCollectionOf<HTMLElement>) {
-    Array.from(petelement).forEach((pet) => {
-      setTimeout(() => {this.setPetOpacity(pet)}, this.petAppearInterval);
-      this.petAppearInterval += 200;
-      console.log(this.petAppearInterval);
-    });
+    }, 50);
   }
 
   private checkEvrything(petelement: HTMLCollectionOf<Element>) {
@@ -87,7 +83,16 @@ export class ShelterpetsComponent implements OnInit, OnChanges {
       clearInterval(this.intervalCheck);
     }
   }
-  private intervalStart():void{
+  
+  private conditionCheck(elements: HTMLCollectionOf<Element>): boolean {
+    return (
+      this.pets !== undefined &&
+      this.pets.length > 0 &&
+      elements.length === this.pets.length
+    );
+  }
+  
+  private intervalStart(): void {
     this.intervalCheck = setInterval(() => {
       const elements: HTMLCollectionOf<Element> =
         document.getElementsByClassName('pet');
@@ -96,5 +101,8 @@ export class ShelterpetsComponent implements OnInit, OnChanges {
       }
     }, 50);
   }
-  
+
+  private setPetOpacity(pet: HTMLElement) {
+    pet.classList.add('appear');
+  }
 }
