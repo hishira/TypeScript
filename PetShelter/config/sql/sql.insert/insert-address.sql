4bedc39f73d747ccc98efc79eff044591b746b45
hishira <okami0124@gmail.com>
2022-06-28 00:26:22 +0200

    update database

---
+++ PetShelter/config/sql/sql.insert/insert-address.sql
@@ -0,0 +1,5 @@
+INSERT INTO address 
+(city, address,country, lat,lng, center_id) VALUES
+("New York", 'Bulvard avenue 52', "USA", 40.730610, -73.935242,1),
+("Amsterdam", 'Santa monica 2/52', "Netherlands", 52.377956, 4.897070,2),
+("Cracov", 'Kazimierza 42', "Poland", 50.049683,19.944544,3);
