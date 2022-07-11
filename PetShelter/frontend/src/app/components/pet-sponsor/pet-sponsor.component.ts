0487722b6eb8e24c6f4b5a574301ec2b2c2780c8
hishira <okami0124@gmail.com>
2022-07-12 00:18:36 +0200

    Start to improve pet support component

--- PetShelter/frontend/src/app/components/pet-sponsor/pet-sponsor.component.ts
+++ PetShelter/frontend/src/app/components/pet-sponsor/pet-sponsor.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { Pet } from "src/app/models/pet.model";
 import { PetService } from "src/app/services/pet.service";
-
+import { getFullAddress } from "src/app/models/address.models";
 @Component({
     selector: 'app-pet-sponsor',
     styleUrls: ['./pet-sponsor.scss'],
     templateUrl: './pet-sponsor.component.html'
 })
 export class PetSponsorComponent implements OnInit{
-    
+    fullAddress: string = '';
+    pet: Pet;
+    shelterLink: string = ''
     constructor(
         private router: ActivatedRoute,
         private petService: PetService
@@ -17,7 +20,10 @@ export class PetSponsorComponent implements OnInit{
     ngOnInit(): void {
         const id = this.router.snapshot.params['id']
         this.petService.getPetById(id).subscribe((pet)=>{
-            console.log(pet);
+           this.pet = pet as Pet;
+           console.log(pet)
+           this.fullAddress = getFullAddress(pet.center.address)
+            this.shelterLink = `/shelter/${this.pet.center.id}`
         })
     }
 }
