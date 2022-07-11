2eda2b583583d78be9d37046dfcae1c39f4f5b1e
hishira <okami0124@gmail.com>
2022-07-11 23:43:38 +0200

    fix login page

--- PetShelter/frontend/src/app/components/login/login.component.ts
+++ PetShelter/frontend/src/app/components/login/login.component.ts
@@ -1,28 +1,33 @@
-import { Component, OnInit } from "@angular/core";
-import { Form, FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { ModalService } from "src/app/services/modal.service";
-import { SignUpComponent } from "../signup/signup.component";
+import { Component, OnInit } from '@angular/core';
+import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ModalService } from 'src/app/services/modal.service';
+import { PasswordForgetComponent } from '../password-forget/password-forget.component';
+import { SignUpComponent } from '../signup/signup.component';
 
 @Component({
-    selector: 'app-login',
-    styleUrls: ['./login.component.scss'],
-    templateUrl: './login.component.html'
+  selector: 'app-login',
+  styleUrls: ['./login.component.scss'],
+  templateUrl: './login.component.html',
 })
-export class LoginComponent implements OnInit{
+export class LoginComponent implements OnInit {
+  form: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+    private modalService: ModalService
+  ) {}
+  
+  forgetPassword() {
+    this.modalService.changeBodycontent(PasswordForgetComponent, {});
+  }
+  
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      login: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
 
-    form: FormGroup;
-    constructor(
-        private formBuilder: FormBuilder,
-        private modalService: ModalService,
-        ){}
-    ngOnInit(): void {
-        this.form = this.formBuilder.group({
-            login: ['',Validators.required],
-            password: ['',Validators.required]
-        })
-    }
-
-    signUp(){
-        this.modalService.changeBodycontent(SignUpComponent,{});
-    }
+  signUp() {
+    this.modalService.changeBodycontent(SignUpComponent, {});
+  }
 }
