a3c709cbb99ff33b3f64e83c0c71341236a080dd
hishira <okami0124@gmail.com>
2022-08-13 23:08:40 +0200

    Add button select at support pet page

--- PetShelter/frontend/src/app/components/pet-sponsor/pet-sponsor.component.ts
+++ PetShelter/frontend/src/app/components/pet-sponsor/pet-sponsor.component.ts
@@ -5,38 +5,40 @@ import { getFullAddress } from 'src/app/models/address.models';
 import { Optional } from 'src/app/models/core.models';
 import { PetGQL, PetSchema } from 'src/app/types/types';
 import { Validators } from '@angular/forms';
+import { DocumentService } from 'src/app/services/document.service';
 @Component({
   selector: 'app-pet-sponsor',
   styleUrls: ['./pet-sponsor.scss'],
   templateUrl: './pet-sponsor.component.html',
 })
 export class PetSponsorComponent implements OnInit {
-  
   donationValues: number[] = [5, 20, 50, 100, 500];
   examplePhotoUrl: string = '';
   form: FormGroup;
   fullAddress: string = '';
+  lastClickedButton: HTMLButtonElement | undefined = undefined;
   pet: PetSchema;
-  selectadDonationCost: Optional<number> = {value: undefined};
   shelterLink: string = '';
   constructor(
     private router: ActivatedRoute,
-     private petQuery: PetGQL,
-     private formBuilder: FormBuilder) {}
+    private petQuery: PetGQL,
+    private formBuilder: FormBuilder,
+    private documentService: DocumentService
+  ) {}
 
   ngOnInit(): void {
     const id = this.router.snapshot.params['id'];
     this.form = this.formBuilder.group({
-      confirmEmail: ['',Validators.required],
-      email: ['',Validators.required],
-      firstName: ['',Validators.required],
-      lastName: ['',Validators.required],
+      amount: [0, Validators.required],
+      confirmEmail: ['', Validators.required],
+      email: ['', Validators.required],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
       message: [''],
-
-    })
-    this.form.valueChanges.subscribe((vlaues)=>{
+    });
+    this.form.valueChanges.subscribe((vlaues) => {
       console.log(vlaues);
-    })
+    });
     this.petQuery
       .watch({ petFilter: { id: { eq: parseInt(id) } } })
       .valueChanges.subscribe((result) => {
@@ -50,7 +52,19 @@ export class PetSponsorComponent implements OnInit {
       });
   }
 
-  selectDonationCost(donationCost: number): void{
-    this.selectadDonationCost.value = donationCost;
+  selectDonationCost(donationCost: number, button: HTMLButtonElement): void {
+    this.lastClickedButton &&
+      this.documentService.togglElementClasses(
+        this.lastClickedButton,
+        'btn-primary',
+        'btn-outline-primary'
+      );
+    this.form.patchValue({ amount: donationCost });
+    this.documentService.togglElementClasses(
+      button,
+      'btn-primary',
+      'btn-outline-primary'
+    );
+    this.lastClickedButton = button;
   }
 }
--- PetShelter/frontend/src/app/services/document.service.ts
+++ PetShelter/frontend/src/app/services/document.service.ts
@@ -7,8 +7,14 @@ export class DocumentService {
 
     constructor(){}
 
-    changeTagCssProperty(tagName: string, cssProperty: string, cssValue: string) {
+    changeTagCssProperty(tagName: string, cssProperty: string, cssValue: string): void {
         const documenttag = document.getElementsByTagName(tagName)[0] as HTMLElement;
         documenttag.setAttribute('style', `${cssProperty}:${cssValue}`);
+    }
+
+    togglElementClasses<T extends HTMLElement>(htmlElement: T, ...cssClassses: string[]): void{
+        for(let cssClass of cssClassses){
+            htmlElement.classList.toggle(cssClass)
+        }
     }
 }
