43ff2c768a1c8903ffb1f2ef4c7da87609fb48f4
hishira <okami0124@gmail.com>
2023-02-05 22:21:48 +0100

    Change export to promise

--- PasswordMenager/backend/src/controllers/export.controller.ts
+++ PasswordMenager/backend/src/controllers/export.controller.ts
@@ -1,24 +1,30 @@
-import { Controller, Get, Request, Res, UseGuards } from "@nestjs/common";
-import { AuthGuard } from "@nestjs/passport";
-import { Response } from "express";
-import { EntryService } from "src/services/entry.service";
-import {writeFileSync} from 'fs'
+import { Controller, Get, Request, Res, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Response } from 'express';
+import { EntryService } from 'src/services/entry.service';
+import { writeFileSync } from 'fs';
+// TODO: Check if work as expected witth promise
 @Controller('export')
-export class ExportController{
-    constructor(private readonly entryService: EntryService){}
+export class ExportController {
+  constructor(private readonly entryService: EntryService) {}
 
-    @Get('csv')
-    @UseGuards(AuthGuard('accessToken'))
-    async getCsv(@Request() req ,@Res() response: Response) {
-        console.log(req.user);
-        const entries = await this.entryService.getByUser(req.user.id);
-        let csvData = ['title','password', 'note'].join(', ') + '\r\n'
-        entries.forEach((entry)=>{
-            csvData+=[entry.title, entry.password, entry.note].join(',')+'\r\n';
+  @Get('csv')
+  @UseGuards(AuthGuard('accessToken'))
+  async getCsv(@Request() req, @Res() response: Response) {
+    console.log(req.user);
+    //const entries = await
+    this.entryService.getByUser(req.user.id).then((resp) => {
+      let csvData = ['title', 'password', 'note'].join(', ') + '\r\n';
+      resp.forEach((entry) => {
+        csvData += [entry.title, entry.password, entry.note].join(',') + '\r\n';
+      });
+      response
+        .set({
+          'Content-Type': 'text/csv',
+          'Content-Disposition': `attachment; filename="users.csv"`,
         })
-        response.set({
-            "Content-Type": "text/csv",
-            "Content-Disposition": `attachment; filename="users.csv"`,
-        }).send(csvData);
-    }
+        .attachment('users.csv')
+        .send(csvData);
+    });
+  }
 }
