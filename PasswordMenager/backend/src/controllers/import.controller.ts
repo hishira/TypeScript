ad06128d29e9a7652308b4670f9b66dea16da7f5
hishira <okami0124@gmail.com>
2023-02-08 18:21:24 +0100

    Update nest to v9

--- PasswordMenager/backend/src/controllers/import.controller.ts
+++ PasswordMenager/backend/src/controllers/import.controller.ts
@@ -1,17 +1,28 @@
 import {
   Controller,
+  ParseFilePipeBuilder,
   Post,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
+import { Multer } from 'multer';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { tap } from 'rxjs';
 @Controller('import')
 export class ImportController {
   constructor() {}
 
   @Post('csv')
   @UseInterceptors(FileInterceptor('file'))
-  uploadFile(@UploadedFile() file) {
+  uploadFile(
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({ fileType: 'csv' })
+        .addMaxSizeValidator({ maxSize: 10000 })
+        .build(),
+    )
+    file: Express.Multer.File,
+  ) {
     console.log(file);
   }
 }
