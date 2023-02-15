129ba2b17433dde9ace178637764f1497707ba37
hishira <okami0124@gmail.com>
2023-02-15 22:29:54 +0100

    Try to implement post find function

--- PasswordMenager/backend/src/schemas/entry.schema.ts
+++ PasswordMenager/backend/src/schemas/entry.schema.ts
@@ -2,7 +2,7 @@ import * as mognoose from 'mongoose';
 import * as crypto from 'crypto';
 import { IEntry } from './Interfaces/entry.interface';
 
-const algorithm = 'aes-256-ctr'
+export const algorithm = 'aes-256-ctr';
 const EntrySchema = new mognoose.Schema({
   title: {
     type: String,
@@ -25,12 +25,37 @@ const EntrySchema = new mognoose.Schema({
     default: '',
   },
 });
-EntrySchema.pre<IEntry>('save', function(next){
+EntrySchema.pre<IEntry>('save', function (next) {
   const encryptedPasswod = this.password;
-  const cipher = crypto.createCipheriv(algorithm, process.env.secretkey, process.env.iv);
-  const encrypted = Buffer.concat([cipher.update(encryptedPasswod), cipher.final()])
+  const cipher = crypto.createCipheriv(
+    algorithm,
+    process.env.secretkey,
+    process.env.iv,
+  );
+  const encrypted = Buffer.concat([
+    cipher.update(encryptedPasswod),
+    cipher.final(),
+  ]);
   this.password = encrypted.toString('hex');
   console.log(this.password);
   next();
-})
+});
+EntrySchema.post('find', function (result) {
+  
+  const resultTmp = result.map((res)=>{
+    console.log(res._doc)
+    const encryptedPassword = res.password;
+    const decipher = crypto.createDecipheriv(algorithm,process.env.secretkey, process.env.iv);
+    const decryptedPassword = Buffer.concat([decipher.update(Buffer.from(encryptedPassword,'hex')),decipher.final()]);
+    console.log(decryptedPassword.toString());
+    return {
+      ...res,
+      password: decryptedPassword.toString()
+    };
+    
+  });
+  //console.log(resultTmp);
+  result = resultTmp
+  return resultTmp;
+});
 export default EntrySchema;
