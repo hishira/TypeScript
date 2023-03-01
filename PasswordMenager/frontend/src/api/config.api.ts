e820cb88480d8092d23e80806c3fdf70d2738bcb
hishira <okami0124@gmail.com>
2023-03-01 16:16:25 +0100

    Seperate api function to class

--- PasswordMenager/frontend/src/api/config.api.ts
+++ PasswordMenager/frontend/src/api/config.api.ts
@@ -1,7 +1,94 @@
-const getUrl = (part: string): string => {
+class ConfigApi {
+  static getUrl = (part: string): string => {
+    return `http://localhost:8080/${part}`;
+  };
+  static frontURL: string = "http://localhost:3000";
+  static readonly fetchObject: RequestInit = {
+    mode: "cors",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": ConfigApi.frontURL,
+    },
+    credentials: "include",
+    method: "GET",
+  };
+}
+
+abstract class Api {
+  static getAuthorizationToken(token: string): string {
+    return `Bearer ${token}`;
+  }
+
+  fetchPostObjectWithToken(obj: any, token: string): RequestInit {
+    return {
+      mode: "cors",
+      headers: {
+        Authorization: Api.getAuthorizationToken(token),
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": ConfigApi.frontURL,
+      },
+      body: JSON.stringify(obj),
+      method: "POST",
+    };
+  }
+
+  fetchPostObject(obj: any): RequestInit {
+    return {
+      mode: "cors",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": ConfigApi.frontURL,
+      },
+      body: JSON.stringify(obj),
+      method: "POST",
+    };
+  }
+
+  fetchGetObjectWithtoken(token: string): RequestInit {
+    return {
+      mode: "cors",
+      headers: {
+        Authorization: Api.getAuthorizationToken(token),
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": ConfigApi.frontURL,
+      },
+      method: "GET",
+    };
+  }
+  fetchDeleteObjectWithToken(token: string): RequestInit {
+    return {
+      mode: "cors",
+      headers: {
+        Authorization: Api.getAuthorizationToken(token),
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": ConfigApi.frontURL,
+      },
+      method: "DELETE",
+    };
+  }
+  fetchPutObjectWithToken(body: object, token: string): RequestInit {
+    return {
+      mode: "cors",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": ConfigApi.frontURL,
+      },
+      body: JSON.stringify(body),
+      method: "PUT",
+    };
+  }
+}
+
+const getUrl =  (part: string): string => {
   return `http://localhost:8080/${part}`;
 };
-
 const fetchObject: RequestInit = {
   mode: "cors",
   headers: {
