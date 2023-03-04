be2dd9f1783cd50f4bf5a3bb89b5ea5d079afef0
hishira <okami0124@gmail.com>
2023-03-04 20:15:33 +0100

    Create auth utils class

--- PasswordMenager/frontend/src/utils/auth.utils.ts
+++ PasswordMenager/frontend/src/utils/auth.utils.ts
@@ -1,32 +1,84 @@
 import { AuthApi } from "../api/auth.api";
 import { getRefreshToken, setAccessToken } from "./localstorage.utils";
-const LoginUser = async (authinfo: UserAuth): Promise<AuthTokens | any> => {
-  return await AuthApi.getInstance()
-    .login(authinfo)
-    .then((resp: Response) => {
-      return resp.json();
-    });
-};
 
-const LoginUserHandle = async (authobj: UserAuth): Promise<LoginReponse> => {
-  const response: AuthTokens = await LoginUser(authobj);
+export class Auth {
+  private static instance: Auth | null = null;
 
-  if (response?.access_token !== "" && response?.refresh_token !== "") {
-    return { status: true, response: response };
+  static getInstance(): Auth {
+    if (this.instance === null) {
+      this.instance = new Auth();
+      return this.instance;
+    }
+    return this.instance;
   }
-  return { status: false, response: response };
-};
-const registerUser = async (
-  signupinfo: UserAuth
-): Promise<null | object | boolean> => {
-  const response: boolean | object = await AuthApi.getInstance()
-    .signup(signupinfo)
-    .then((resp: Response) => {
-      if (resp.status === 200 || resp.status === 201) return resp.json();
-      return false;
-    });
-  return response === false ? null : response;
-};
+
+  async LoginUser(authinfo: UserAuth): Promise<AuthTokens | any> {
+    return await AuthApi.getInstance()
+      .login(authinfo)
+      .then((resp: Response) => {
+        return resp.json();
+      });
+  }
+
+  async LoginUserHandle(authobj: UserAuth): Promise<LoginReponse> {
+    const response: AuthTokens = await this.LoginUser(authobj);
+
+    if (response?.access_token !== "" && response?.refresh_token !== "") {
+      return { status: true, response: response };
+    }
+    return { status: false, response: response };
+  }
+
+  async registerUser(signupinfo: UserAuth): Promise<null | object | boolean> {
+    const response: boolean | object = await AuthApi.getInstance()
+      .signup(signupinfo)
+      .then((resp: Response) => {
+        if (resp.status === 200 || resp.status === 201) return resp.json();
+        return false;
+      });
+    return response === false ? null : response;
+  }
+
+  async refreshToken(): Promise<void> {
+    const refreshtoken: string = getRefreshToken();
+    const response: number | AccessToken = await AuthApi.getInstance()
+      .refreshAccessToken(refreshtoken)
+      .then((resp: Response) => {
+        if (resp.status === 201 || resp.status === 200) return resp.json();
+        return 401;
+      });
+    if ((response as AccessToken).access_token !== "") {
+      setAccessToken((response as AccessToken).access_token);
+    }
+  }
+}
+//const LoginUser = async (authinfo: UserAuth): Promise<AuthTokens | any> => {
+//  return await AuthApi.getInstance()
+//    .login(authinfo)
+//    .then((resp: Response) => {
+//      return resp.json();
+//    });
+//};
+
+//const LoginUserHandle = async (authobj: UserAuth): Promise<LoginReponse> => {
+//  const response: AuthTokens = await LoginUser(authobj);
+
+//  if (response?.access_token !== "" && response?.refresh_token !== "") {
+//    return { status: true, response: response };
+//  }
+//  return { status: false, response: response };
+//};
+//const registerUser = async (
+//  signupinfo: UserAuth
+//): Promise<null | object | boolean> => {
+//  const response: boolean | object = await AuthApi.getInstance()
+//    .signup(signupinfo)
+//    .then((resp: Response) => {
+//      if (resp.status === 200 || resp.status === 201) return resp.json();
+//      return false;
+//    });
+//  return response === false ? null : response;
+//};
 const refreshToken = async (): Promise<void> => {
   const refreshtoken: string = getRefreshToken();
   const response: number | AccessToken = await AuthApi.getInstance()
@@ -40,4 +92,4 @@ const refreshToken = async (): Promise<void> => {
   }
 };
 
-export { LoginUserHandle, refreshToken, registerUser };
+export {  refreshToken };
