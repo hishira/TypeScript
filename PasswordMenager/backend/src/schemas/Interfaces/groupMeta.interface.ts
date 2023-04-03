f09d0d6b3644070be2e93471539ef41d34fcafad
hishira <okami0124@gmail.com>
2023-04-03 18:07:23 +0200

    Add groupmeta object to group schema

---
+++ PasswordMenager/backend/src/schemas/Interfaces/groupMeta.interface.ts
@@ -0,0 +1,5 @@
+import { IMeta } from './meta.interface';
+
+export interface IGroupMeta extends IMeta {
+  readonly lastName: string;
+}
--- PasswordMenager/backend/src/schemas/group.schema.ts
+++ PasswordMenager/backend/src/schemas/group.schema.ts
@@ -1,14 +1,24 @@
 import * as mongoose from 'mongoose';
 import { IGroup } from './Interfaces/group.interface';
+import GroupMetaSchema from './groupMeta.schema';
 
-const GroupSchema = new mongoose.Schema({
+const GroupSchema = new mongoose.Schema<IGroup>({
   name: {
     type: String,
     required: true,
+    set: function (name: string) {
+      this._name = this.name;
+      return name;
+    },
   },
   userid: {
     type: String,
     required: true,
+  },
+  meta: {
+    type: GroupMetaSchema,
+    requred: true,
+    default: () => ({}),
   },
 });
 
