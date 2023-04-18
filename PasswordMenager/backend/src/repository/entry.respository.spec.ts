e9f6099ffcc2cbcb09585f292b1c4eebb7875fab
hishira <okami0124@gmail.com>
2023-04-18 18:47:48 +0200

    update test for entry repository

--- PasswordMenager/backend/src/repository/entry.respository.spec.ts
+++ PasswordMenager/backend/src/repository/entry.respository.spec.ts
@@ -3,7 +3,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { Model } from 'mongoose';
 import { IEntry } from 'src/schemas/Interfaces/entry.interface';
 import { EntryRepository } from './entry.repository';
-import { EntryMockModel } from '../../test/mock/EntryMock';
+import { EntryMockModel, entryMock } from '../../test/mock/EntryMock';
 
 describe('EntryRepository', () => {
   let entryModel: Model<IEntry>;
@@ -24,15 +24,37 @@ describe('EntryRepository', () => {
     entryModel = module.get<Model<IEntry>>('ENTRY_MODEL');
     entryRepo = module.get<EntryRepository>(EntryRepository);
   });
+
+  it('Entry model should be defined', () => {
+    expect(entryModel).toBeDefined();
+  });
+
   it('Entry repo should be definied', () => {
     expect(entryRepo).toBeDefined();
   });
-  it('while create should use model ', async () => {
+
+  it('Created entry should have default entry property:  username, password, note and title', async () => {
     const user = await entryRepo.create({
-      toObject: () => ({ name: 'tea', template: 'asd' }),
+      toObject: () => ({ ...entryMock() }),
     });
-    expect(user).toHaveProperty('name');
+    expect(user).toHaveProperty('title');
+    expect(user).toHaveProperty('username');
+    expect(user).toHaveProperty('password');
+    expect(user).toHaveProperty('note');
   });
+
+  it('Created entry should have meta, with specific properties', async () => {
+    const user = await entryRepo.create({
+      toObject: () => ({ ...entryMock() }),
+    });
+    expect(user).toHaveProperty('meta');
+    expect(user.meta).toHaveProperty('lastNote');
+    expect(user.meta).toHaveProperty('lastPassword');
+    expect(user.meta).toHaveProperty('lastTitle');
+    expect(user.meta).toHaveProperty('lastUsername');
+    expect(user.meta).toHaveProperty('lastEditedVariable');
+  });
+
   it('entry repo should use entry module', async () => {
     const spy = jest.spyOn(entryModel, 'findOne').mockReturnValueOnce({
       exec: jest.fn().mockResolvedValueOnce({ name: 'other' }),
--- PasswordMenager/backend/test/mock/EntryMock.ts
+++ PasswordMenager/backend/test/mock/EntryMock.ts
@@ -1,15 +1,44 @@
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { IEntry } from 'src/schemas/Interfaces/entry.interface';
-
+export const entryMock = (entry?: IEntry) =>
+  entry ?? {
+    _id: new Types.ObjectId(32), //ObjectId(32),
+    title: 'test title',
+    userid: 'asd123cxz',
+    username: 'test username',
+    password: 'dupa123',
+    note: 'dupa',
+    groupid: new Types.ObjectId(32),
+    passwordExpiredDate: Date.now(),
+    meta: {
+      crateDate: Date.now(),
+      firstEditDate: Date.now(),
+      editDate: Date.now(),
+      lastNote: null,
+      lastPassword: null,
+      lastTitle: null,
+      lastUsername: null,
+      lastEditedVariable: null,
+    },
+  };
 export class EntryMockModel {
-  private test;
-  constructor(private tt) {
-    this.test = tt;
-  }
-  save = jest.fn().mockResolvedValue({});
+  constructor(private data) {}
+  save() {
+    return this.data;
+  } //= jest.fn().mockResolvedValue(Promise.resolve(this.data));
   static exec = jest.fn();
   static find = jest.fn().mockResolvedValue({});
   static findOne = jest.fn().mockResolvedValue({});
   static findOneAndUpdate = jest.fn().mockResolvedValue({});
   static deleteOne = jest.fn().mockResolvedValue(true);
+
+  static getValues = () => ({
+    new: (val) => val, //jest.fn().mockResolvedValue(entryMock()),
+    constructor: (val) => val, //jest.fn().mockResolvedValue(entryMock()),
+    exec: jest.fn(),
+    find: jest.fn().mockResolvedValue({}),
+    findOne: jest.fn().mockResolvedValue({}),
+    findOneAndUpdate: jest.fn().mockResolvedValue({}),
+    deleteOne: jest.fn().mockResolvedValue(true),
+  });
 }
