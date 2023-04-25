b30250499034060e7d426ae2ecdaea3ae31f6727
hishira <okami0124@gmail.com>
2023-04-25 18:11:05 +0200

    Add more group service test

--- PasswordMenager/backend/src/services/group.service.spec.ts
+++ PasswordMenager/backend/src/services/group.service.spec.ts
@@ -11,7 +11,8 @@ import { TestUtils } from '../../test/utils/TestUtils';
 
 describe('GroupService', () => {
   let groupService: GroupService;
-
+  let entryService: EntryService;
+  let groupRepository: GroupRepository;
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
@@ -33,6 +34,7 @@ describe('GroupService', () => {
       ],
     }).compile();
     groupService = module.get<GroupService>(GroupService);
+    entryService = module.get<EntryService>(EntryService);
   });
 
   beforeEach(() => jest.clearAllMocks());
@@ -40,6 +42,14 @@ describe('GroupService', () => {
   it('Group service should be defined', () => {
     expect(groupService).toBeDefined();
   });
+
+  it('Entry service should be defined', () => {
+    expect(entryService).toBeDefined();
+  });
+
+  //it('Group repositoru should be defined', () => {
+  //  expect(groupRepository).toBeDefined();
+  //});
 
   it('Create method should create group', async () => {
     const id = new Types.ObjectId(32).toString();
@@ -66,5 +76,24 @@ describe('GroupService', () => {
     );
 
     TestUtils.expectHasProperties(group, 'name');
+  });
+
+  it('deleteGroup should return promise', () => {
+    const deletePromise = groupService.deleteGroup(
+      new Types.ObjectId(32).toString(),
+    );
+    expect(deletePromise).resolves.toBeDefined();
+  });
+
+  it('deleteGroup should use deleteByGroup from entryty service nad grouprepository delete', () => {
+    const spy1 = jest.spyOn(entryService, 'deleteByGroup');
+    // TODO: Check if we can retrive groupRepository
+    //const spy2 = jest.spyOn(groupRepository, 'delete');
+    const deletePromise = groupService.deleteGroup(
+      new Types.ObjectId(32).toString(),
+    );
+    expect(spy1).toBeCalledTimes(1);
+    //expect(spy2).toBeCalledTimes(1);
+    expect(deletePromise).resolves.toBeDefined();
   });
 });
