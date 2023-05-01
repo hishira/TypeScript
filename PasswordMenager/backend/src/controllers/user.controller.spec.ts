516ac84fc2ecd8cbb593bc7b74f2878f89d1804e
hishira <okami0124@gmail.com>
2023-05-01 18:17:49 +0200

    update user controller

--- PasswordMenager/backend/src/controllers/user.controller.spec.ts
+++ PasswordMenager/backend/src/controllers/user.controller.spec.ts
@@ -2,8 +2,13 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UserRepository } from 'src/repository/user.repository';
 import { Repository } from 'src/schemas/Interfaces/repository.interface';
 import { UserService } from 'src/services/user.service';
-import { UserModelMock } from '../../test/mock/UserModelMock';
+import {
+  EditUserDtoMock,
+  UserModelMock,
+  UserRequestMock,
+} from '../../test/mock/UserModelMock';
 import { UsersController } from './user.contaoller';
+import { TestUtils } from '../../test/utils/TestUtils';
 
 describe('UserController', () => {
   let userController: UsersController;
@@ -28,12 +33,99 @@ describe('UserController', () => {
     userService = module.get<UserService>(UserService);
   });
 
+  beforeEach(() => jest.clearAllMocks());
   describe('Should be defined', () => {
     it('User controller should be defined', () => {
       expect(userController).toBeDefined();
     });
     it('User service should be defined', () => {
       expect(userService).toBeDefined();
+    });
+  });
+
+  describe('Find all', () => {
+    it('Should use getAll from userService', () => {
+      const spy = jest.spyOn(userService, 'getAll');
+      userController.findAll();
+
+      expect(spy).toBeCalledTimes(1);
+    });
+
+    it('Should retunr promise', async () => {
+      const response = userController.findAll();
+
+      expect(response).resolves.toBeDefined();
+    });
+
+    it('Should return users', async () => {
+      const response = await userController.findAll();
+      TestUtils.expectHasProperties(response, 'login', 'password');
+    });
+  });
+
+  describe('Get one', () => {
+    it('Should use getAll from userService', () => {
+      const spy = jest.spyOn(userService, 'getOne');
+      userController.findOne();
+
+      expect(spy).toBeCalledTimes(1);
+    });
+
+    it('Should retunr promise', async () => {
+      const response = userController.findOne();
+
+      expect(response).resolves.toBeDefined();
+    });
+
+    it('Should return users', async () => {
+      const response = await userController.findOne();
+      TestUtils.expectHasProperties(response, 'login', 'password');
+    });
+  });
+
+  describe('Get one', () => {
+    it('Should use getAll from userService', () => {
+      const spy = jest.spyOn(userService, 'getOne');
+      userController.findOne();
+
+      expect(spy).toBeCalledTimes(1);
+    });
+
+    it('Should retunr promise', async () => {
+      const response = userController.findOne();
+
+      expect(response).resolves.toBeDefined();
+    });
+
+    it('Should return users', async () => {
+      const response = await userController.findOne();
+      TestUtils.expectHasProperties(response, 'login', 'password');
+    });
+  });
+
+  describe('UpdateUser', () => {
+    it('Should use getAll from userService', () => {
+      const spy = jest.spyOn(userService, 'update');
+      userController.updateUser(EditUserDtoMock(), UserRequestMock());
+
+      expect(spy).toBeCalledTimes(1);
+    });
+
+    it('Should retunr promise', async () => {
+      const response = userController.updateUser(
+        EditUserDtoMock(),
+        UserRequestMock(),
+      );
+
+      expect(response).resolves.toBeDefined();
+    });
+
+    it('Should return users', async () => {
+      const response = await userController.updateUser(
+        EditUserDtoMock(),
+        UserRequestMock(),
+      );
+      TestUtils.expectHasProperties(response, 'login', 'password');
     });
   });
 });
