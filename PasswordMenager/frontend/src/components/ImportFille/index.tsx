c3ebcae89c451156b025a3433ee82e3506b63af3
hishira <okami0124@gmail.com>
2023-05-06 16:25:27 +0200

    Accept button work

--- PasswordMenager/frontend/src/components/ImportFille/index.tsx
+++ PasswordMenager/frontend/src/components/ImportFille/index.tsx
@@ -1,6 +1,5 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { ImportContainer, ImportInput } from "./component.styled";
-import { Import } from "../../utils/import.utils";
 type ImportFileProps = {
   fileChangeHandle: (...args: File[]) => void;
 };
@@ -9,23 +8,18 @@ export const ImportFile = ({
 }: ImportFileProps): JSX.Element => {
   const [file, setFile] = useState<File>();
   const fileChange = (e: ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.files);
-    if (e.target.files && e.target.files.length <= 0) return;
-    e.target.files && setFile(e.target.files[0]);
-    file && fileChangeHandle(file);
-    //if (file) {
-    //  const formData = new FormData();
-    //  formData.set("file", file);
-    //  Import.getInstance()
-    //    .ImportFile(formData, file.size)
-    //    .then(console.log)
-    //    .catch(console.log);
-    //}
+    const { target: { files = [] } = {} } = e;
+    if (files && files.length <= 0) return;
+    files && setFile(files[0]);
   };
+  useEffect(() => {
+    if (!file) return;
+    fileChangeHandle(file);
+  }, [file]);
   return (
     <ImportContainer>
       <div></div>
-      <ImportInput onChange={fileChange}></ImportInput>
+      <ImportInput onChange={(e) => fileChange(e)}></ImportInput>
     </ImportContainer>
   );
 };
--- PasswordMenager/frontend/src/components/ImportModals/ImportDecrypted/index.tsx
+++ PasswordMenager/frontend/src/components/ImportModals/ImportDecrypted/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
-import { AcceptModalComponent } from "../../Modal/AcceptModal";
+import { Import } from "../../../utils/import.utils";
 import { ImportFile } from "../../ImportFille";
+import { AcceptModalComponent } from "../../Modal/AcceptModal";
 type ImportDecryptedProps = {
   modalOpen: boolean;
   closeModalHandle: () => void;
@@ -10,23 +11,42 @@ export const ImportDecrypted = ({
   closeModalHandle,
 }: ImportDecryptedProps): JSX.Element => {
   const [importModalOpen, setImportModalOpen] = useState<boolean>(modalOpen);
+  const [formData, setFormData] = useState<FormData>();
+  const [uuid, setuuid] = useState<string>("");
   const closeImportModalHandle = (): void => {
     setImportModalOpen(false);
     closeModalHandle();
+    fileChange();
   };
   const fileChange = (...args: File[]): void => {
-    console.log(args);
+    if (args.length <= 0) return;
+    const file = args[0];
+    if (file) {
+      const formData = new FormData();
+      formData.set("file", file);
+      setFormData(formData);
+    }
+  };
+
+  const acceptHandleFunction = () => {
+    if (!formData) return;
+    Import.getInstance()
+      .ImportFile(formData, 0)
+      .then(console.log)
+      .catch(console.log);
   };
 
   useEffect(() => {
     setImportModalOpen(modalOpen);
+    setuuid((Math.random() + 1).toString(36).substring(7));
   }, [modalOpen]);
+
   return (
     <AcceptModalComponent
       visible={importModalOpen}
       onClose={closeImportModalHandle}
-      component={<ImportFile fileChangeHandle={fileChange} />}
-      acceptHandle={() => {}}
+      component={<ImportFile fileChangeHandle={fileChange} key={uuid} />}
+      acceptHandle={acceptHandleFunction}
     />
   );
 };
