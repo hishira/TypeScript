e42ebf9e6ebd15576c80f537007cf001280b289b
hishira <okami0124@gmail.com>
2023-05-16 18:08:19 +0200

    Update popup tests

--- PasswordMenager/frontend/__tests__/PopUpElement-test.tsx
+++ PasswordMenager/frontend/__tests__/PopUpElement-test.tsx
@@ -1,7 +1,19 @@
 import { render } from "@testing-library/react";
 import { Provider } from "mobx-react";
-import { IGeneral } from "../src/models/General";
+import { General, IGeneral } from "../src/models/General";
 import PopUpElement from "../src/components/Popup/index";
+
+const POPUPMESSAGE = "Example popup message";
+const POPUPTYPE = "error";
+const getStoreForPopUp = () =>
+  General.create({
+    useractive: true,
+    popUpelement: {
+      open: true,
+      type: POPUPTYPE,
+      message: POPUPMESSAGE,
+    },
+  });
 const getContainer = (store?: IGeneral): HTMLElement => {
   const { container } = render(
     <Provider store={store}>
@@ -11,8 +23,21 @@ const getContainer = (store?: IGeneral): HTMLElement => {
   return container;
 };
 
+// TODO: MORE ? 
 describe("PopUpElement component", () => {
   it("Container should be defined", () => {
     expect(getContainer()).toBeDefined();
+  });
+
+  it("Should container proper type message", () => {
+    const container = getContainer(getStoreForPopUp());
+    const elements = container.querySelectorAll("div");
+    expect(elements[1].textContent).toBe(POPUPTYPE.toUpperCase());
+  });
+
+  it("Should container proper message", () => {
+    const container = getContainer(getStoreForPopUp());
+    const elements = container.querySelectorAll("div");
+    expect(elements[2].textContent).toBe(POPUPMESSAGE);
   });
 });
