d82f6393ca2c93b5e01995744bb06608f659e06f
hishira <okami0124@gmail.com>
2023-05-19 18:40:35 +0200

    Create tests for password table

---
+++ PasswordMenager/frontend/__tests__/PasswordTableComponent-test.tsx
@@ -0,0 +1,45 @@
+import { cleanup, render } from "@testing-library/react";
+import { PasswordTableComponent } from "../src/components/PasswordTable";
+import { GetEntriesMock } from "./utils/Entry.mock";
+
+const deleteHandleMockFunction = jest.fn(() => {});
+const oneEditHandleMockFunction = jest.fn(() => {});
+const moreClickHandleMockFunction = jest.fn(() => {});
+const getContainer = (numberOfEntries: number = 10): HTMLElement => {
+  const { container } = render(
+    <PasswordTableComponent
+      entries={GetEntriesMock(numberOfEntries)}
+      deletehandle={deleteHandleMockFunction}
+      onedithandle={oneEditHandleMockFunction}
+      moreClickHandle={moreClickHandleMockFunction}
+    />
+  );
+
+  return container;
+};
+afterEach(cleanup);
+afterEach(() => jest.clearAllMocks());
+
+describe('PasswordTableComponent component',()=>{
+    it('Component should be defined', ()=>{
+        expect(getContainer()).toBeDefined();
+    })
+
+    it('Should has table component', ()=>{
+        const table = getContainer().querySelector('table')
+
+        expect(table).toBeDefined();
+    })
+
+    it('Should has table head', ()=>{
+        const thead = getContainer().querySelector('thead');
+
+        expect(thead).toBeDefined();
+    })
+
+    it('Should has 5 tablehead element', ()=>{
+        const theads = getContainer().querySelectorAll('th');
+
+        expect(theads).toHaveLength(5);
+    })
+})
