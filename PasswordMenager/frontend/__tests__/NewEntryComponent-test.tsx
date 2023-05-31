005ed2dc8903a2941105dfffa0d0cd310d73adbf
hishira <okami0124@gmail.com>
2023-05-31 20:10:57 +0200

    Add more tests for NewEntryComponent

--- PasswordMenager/frontend/__tests__/NewEntryComponent-test.tsx
+++ PasswordMenager/frontend/__tests__/NewEntryComponent-test.tsx
@@ -1,10 +1,12 @@
-import { cleanup, render, waitFor } from "@testing-library/react";
+import { cleanup, fireEvent, render, waitFor } from "@testing-library/react";
 import NewEntryComponent from "../src/components/NewEntryComponent";
 import { randomUUID } from "./utils/uuid.utils";
 import { GetGroupsMock } from "./utils/Group.mock";
 import { Group } from "../src/utils/group.utils";
 import { Entry } from "../src/utils/entry.utils";
 import { GetEntryMock } from "./utils/Entry.mock";
+import { checkElementMatchContent } from "./utils/element.utils";
+import { getButtonWithSpecificText } from "./utils/button.utils";
 
 const groupSpy = jest
   .spyOn(Group.prototype, "GetGroupsByUser")
@@ -29,7 +31,7 @@ const editEntrySpyComponent = jest
   );
 const closeModalDispatchMockHandle = jest.fn(() => {});
 const refreshMockFunction = jest.fn(() => {});
-const getContainer = (isEdit: boolean = true): HTMLElement => {
+const getContainer = (isEdit: boolean = false): HTMLElement => {
   const { container } = render(
     <NewEntryComponent
       edit={isEdit}
@@ -61,5 +63,79 @@ describe("NewEntryComponent test", () => {
     await waitFor(() =>
       expect(getContainer().querySelector("input")).toBeDefined()
     );
+  });
+
+  it("Should has 4 labels", async () => {
+    await waitFor(() => {
+      expect(getContainer().querySelectorAll("label")).toHaveLength(4);
+    });
+  });
+
+  it("Labes should has proper text contens", async () => {
+    await waitFor(() => {
+      const labels = getContainer().querySelectorAll("label");
+      checkElementMatchContent(
+        labels ?? [],
+        "Title",
+        "Username",
+        "Password",
+        "Note"
+      );
+    });
+  });
+
+  it("When it is not edit form, it should has Add button", async () => {
+    await waitFor(() => {
+      const buttons = getContainer().querySelectorAll("button");
+      const addButton = getButtonWithSpecificText(buttons, "Add");
+      expect(addButton).toBeDefined();
+    });
+  });
+
+  it("When it is edit form, it should has Update button", async () => {
+    await waitFor(() => {
+      const buttons = getContainer(true).querySelectorAll("button");
+      const update = getButtonWithSpecificText(buttons, "Update");
+      expect(update).toBeDefined();
+    });
+  });
+
+  it("Component should trigger GetGroupsByUser", async () => {
+    await waitFor(() => {
+      getContainer();
+      expect(groupSpy).toBeCalledTimes(1);
+    });
+  });
+
+  it("Click add buttons should trigger CreateNewEntryUser", async () => {
+    await waitFor(() => {
+      const buttons = getContainer().querySelectorAll("button");
+      const addButton = getButtonWithSpecificText(buttons, "Add");
+      addButton && fireEvent.click(addButton);
+      expect(createEntrySpyComponent).toBeCalledTimes(1);
+    });
+  });
+
+  it("Not edit mode should not trigger getEntryById", async () => {
+    await waitFor(() => {
+      getContainer();
+      expect(entrySpy).toBeCalledTimes(0);
+    });
+  });
+
+  it("Edit mode should not trigger getEntryById", async () => {
+    await waitFor(() => {
+      getContainer(true);
+      expect(entrySpy).toBeCalledTimes(1);
+    });
+  });
+
+  it("Click update button should trigger EntryEditById", async () => {
+    await waitFor(() => {
+      const buttons = getContainer(true).querySelectorAll("button");
+      const update = getButtonWithSpecificText(buttons, "Update");
+      update && fireEvent.click(update);
+      expect(editEntrySpyComponent).toBeCalledTimes(1);
+    });
   });
 });
