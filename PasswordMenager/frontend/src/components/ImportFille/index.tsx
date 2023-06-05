d1aaed1f729b02a427c7e13911abf934354f1399
hishira <okami0124@gmail.com>
2023-06-05 18:41:07 +0200

    Change password input at import modal

--- PasswordMenager/frontend/src/components/ImportFille/index.tsx
+++ PasswordMenager/frontend/src/components/ImportFille/index.tsx
@@ -1,5 +1,6 @@
 import { ChangeEvent, useEffect, useState } from "react";
 import { ImportContainer, ImportInput } from "./component.styled";
+import FormElement from "../FormElement";
 type ImportFileProps = {
   fileChangeHandle: (...args: File[]) => void;
 };
@@ -7,10 +8,14 @@ export const ImportFile = ({
   fileChangeHandle,
 }: ImportFileProps): JSX.Element => {
   const [file, setFile] = useState<File>();
+  const [password, setPassword] = useState<string>("");
   const fileChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { target: { files = [] } = {} } = e;
     if (files && files.length <= 0) return;
     files && setFile(files[0]);
+  };
+  const passwordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
   };
   useEffect(() => {
     if (!file) return;
@@ -21,11 +26,19 @@ export const ImportFile = ({
     <ImportContainer>
       <div>
         <label>Choice file</label>
-        <ImportInput role='fileinput' onChange={(e) => fileChange(e)}></ImportInput>
+        <ImportInput
+          role="fileinput"
+          onChange={(e) => fileChange(e)}
+        ></ImportInput>
       </div>
       <div>
-        <label>Password</label>
-        <input type="text" />
+        <FormElement
+          label={"Password"}
+          inputplaceholder="***"
+          inputChange={passwordChange}
+          inputtype="password"
+          value={password}
+        />
       </div>
     </ImportContainer>
   );
