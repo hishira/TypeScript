a61515689778829de183040fdd495fc088e7c6e1
hishira <okami0124@gmail.com>
2023-06-08 14:40:34 +0200

    Fix passbar problem

--- PasswordMenager/frontend/src/components/PassBarr/component.styled.ts
+++ PasswordMenager/frontend/src/components/PassBarr/component.styled.ts
@@ -1,10 +1,13 @@
 import styled from "styled-components";
 
+export const GroupContainer = styled.div`
+  & > button:not(:first-child) {
+    margin-left: 15px;
+  }
+`;
+
 export const Container = styled.div`
   display: flex;
   padding: 5px;
   justify-content: center;
-  & > button:not(:first-child) {
-    margin-left: 15px;
-  }
 `;
