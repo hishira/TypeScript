087078058fa23f018281091a8cc1bb5e7a3a0860
hishira <okami0124@gmail.com>
2023-06-11 16:36:23 +0200

    Add modal to confirm delete entry

--- PasswordMenager/frontend/src/components/FieldsComponent/index.tsx
+++ PasswordMenager/frontend/src/components/FieldsComponent/index.tsx
@@ -8,7 +8,10 @@ import Modal from "../Modal";
 import NewEntryComponent from "../NewEntryComponent";
 import { PasswordTableComponent } from "../PasswordTable";
 import { Container } from "./component.styled";
+import { AcceptModalComponent } from "../Modal/AcceptModal";
 
+//TODO: Refactor, like in group component create modal component for modals view
+const DeleteEntryModal = () => <div>Are you sure to delete entry?</div>;
 const FieldsContainer = ({
   selectedgroup,
   refreshgroupentities,
@@ -17,6 +20,8 @@ const FieldsContainer = ({
   const entries = PasswordEntries(selectedgroup, refreshall);
   const [editmodalopen, seteditmodalopen] = useState<boolean>(false);
   const [entrytoedit, setentrytoedit] = useState<string>("");
+  const [deleteModalOpen, setDeleteModalOpen] = useState<boolean>(false);
+  const [entryToDelete, setEntryToDelete] = useState<string>("");
   const [refreshmodalentry, setrefreshmodalentry] = useState<boolean>(false);
   const [smallmodalopen, setsmallmodalopen] = useState<boolean>(false);
   const [entrywithsmallbutton, setentrywithsmallbutton] =
@@ -24,13 +29,20 @@ const FieldsContainer = ({
 
   ResizeWindowsHandle(setsmallmodalopen, setentrywithsmallbutton);
 
+  const acceptDeleteHandle = () => {
+    Entry.getInstance()
+      .DeleteUserEntry(entryToDelete)
+      .then((response) => {
+        if (response.status) {
+          refreshgroupentities();
+          setDeleteModalOpen(false);
+        }
+      })
+      .catch((e) => e && console.error(e));
+  };
   const deletehandle = async (entryid: string): Promise<void> => {
-    console.log(entryid);
-    const response: DeleteEntryResponse =
-      await Entry.getInstance().DeleteUserEntry(entryid);
-    if (response.status) {
-      refreshgroupentities();
-    }
+    setEntryToDelete(entryid);
+    setDeleteModalOpen(true);
   };
 
   const onmodalclose = (): void => {
@@ -70,6 +82,14 @@ const FieldsContainer = ({
               closeModalDispatcherHandle={seteditmodalopen}
             />
           }
+        />
+      ) : null}
+      {deleteModalOpen ? (
+        <AcceptModalComponent
+          visible={deleteModalOpen}
+          onClose={() => setDeleteModalOpen(false)}
+          acceptHandle={acceptDeleteHandle}
+          component={DeleteEntryModal()}
         />
       ) : null}
       {smallmodalopen ? (
