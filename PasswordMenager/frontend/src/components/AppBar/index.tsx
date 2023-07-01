9bfb3ca2043e427b928d5a5a0359e8eb76026103
hishira <okami0124@gmail.com>
2023-07-01 18:06:06 +0200

    Add Account icon to show button

--- PasswordMenager/frontend/src/components/AppBar/index.tsx
+++ PasswordMenager/frontend/src/components/AppBar/index.tsx
@@ -4,7 +4,15 @@ import { IGeneral, View } from "../../models/General";
 import { SessionStorage } from "../../utils/localstorage.utils";
 import Button from "../Button/index";
 import PassBar from "../PassBarr";
-import { Bar, LeftSide, RigthSide } from "./component.styled";
+import {
+  AccountButtons,
+  AccountView,
+  Bar,
+  LeftSide,
+  RigthSide,
+} from "./component.styled";
+import { AccountIcon } from "../icons/AccountIcon";
+import { useState } from "react";
 
 type AppBarProps = {
   store?: IGeneral;
@@ -15,8 +23,13 @@ type AppBarLeftSideProp = {
 const AppBarLeftSide = ({ userActive }: AppBarLeftSideProp): JSX.Element => {
   return <LeftSide>{userActive ? <PassBar></PassBar> : null}</LeftSide>;
 };
-const AppBar = ({ store }: AppBarProps): JSX.Element => {
+type AccountContainerProps = {
+  store?: IGeneral;
+};
+const AccountContainer = ({ store }: AccountContainerProps) => {
+  const [openAccountMenu, setOpenAccountMenu] = useState<boolean>(false);
   const hisotry = useHistory();
+
   const logouthandle = () => {
     store?.setUserActive(false);
     SessionStorage.getInstance().removeStorage();
@@ -25,18 +38,25 @@ const AppBar = ({ store }: AppBarProps): JSX.Element => {
   const clickHandle = () => {
     store?.setViewType(View.Card);
   };
+
+  return !store?.UserActivity ? null : (
+    <AccountView>
+      <AccountIcon click={() => setOpenAccountMenu(!openAccountMenu)} />
+      <AccountButtons visible={openAccountMenu}>
+        <Button onClick={logouthandle} color="lightblue">
+          Logout
+        </Button>
+        <Button onClick={clickHandle}>Change view</Button>
+      </AccountButtons>
+    </AccountView>
+  );
+};
+const AppBar = ({ store }: AppBarProps): JSX.Element => {
   return (
     <Bar>
       <AppBarLeftSide userActive={store?.UserActivity} />
       <RigthSide>
-        {!store?.UserActivity ? null : (
-          <>
-            <Button onClick={logouthandle} color="lightblue">
-              Logout
-            </Button>
-            <Button onClick={clickHandle}>Change view</Button>
-          </>
-        )}
+        <AccountContainer store={store} />
       </RigthSide>
     </Bar>
   );
