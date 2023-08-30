2fa3b52d1a8a631b12c8977cce68e6b1760a9f43
hishira <okami0124@gmail.com>
2023-08-30 18:47:16 +0200

    Add simple deleted view

--- PasswordMenager/frontend/src/components/AppBar/AccountContainer/AccountInfo/AccountInfoView/LastDeletedElementsCardView/component.styled.ts
+++ PasswordMenager/frontend/src/components/AppBar/AccountContainer/AccountInfo/AccountInfoView/LastDeletedElementsCardView/component.styled.ts
@@ -1,3 +1,11 @@
 import styled from "styled-components";
 
 export const LastDeletedElements = styled.div``
+
+export const Elements = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+export const LastDeletedElement = styled.div`
+`
--- PasswordMenager/frontend/src/components/AppBar/AccountContainer/AccountInfo/AccountInfoView/LastDeletedElementsCardView/index.tsx
+++ PasswordMenager/frontend/src/components/AppBar/AccountContainer/AccountInfo/AccountInfoView/LastDeletedElementsCardView/index.tsx
@@ -1,5 +1,29 @@
-import { LastDeletedElements } from "./component.styled";
+import { useEffect, useState } from "react";
+import {
+  Elements,
+  LastDeletedElement,
+  LastDeletedElements,
+} from "./component.styled";
+import { Entry } from "../../../../../../utils/entry.utils";
 
 export const LastDeletedElementsCardView = () => {
-  return <LastDeletedElements>Last 10 deleted entries</LastDeletedElements>;
+  const [lastDeletedEntries, setLastDeletedEntries] = useState<IEntry[]>([]);
+  useEffect(() => {
+    Entry.getInstance()
+      .getLastDeletedEntries()
+      .then((entries: IEntry[]) => {
+        setLastDeletedEntries(entries);
+      });
+  }, []);
+  return (
+    <LastDeletedElements>
+      Last 10 deleted entries
+      <br />
+      <Elements>
+        {lastDeletedEntries.map((entry) => (
+          <LastDeletedElement>{entry.title}</LastDeletedElement>
+        ))}
+      </Elements>
+    </LastDeletedElements>
+  );
 };
