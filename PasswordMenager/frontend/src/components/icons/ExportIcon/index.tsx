6cb4c7eb8169695592c3f9bec2ba7c240f2af107
hishira <okami0124@gmail.com>
2023-09-02 19:33:10 +0200

    Add export import icons

---
+++ PasswordMenager/frontend/src/components/icons/ExportIcon/index.tsx
@@ -0,0 +1,41 @@
+import { SVGProps } from "react";
+import { IconProps } from "../type";
+
+export const ExportIcon = ({ click }: IconProps): JSX.Element => {
+  const props: SVGProps<SVGSVGElement> = {};
+  if (click) {
+    props["onClick"] = click;
+  }
+  return (
+    <svg {...props} viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg">
+      <rect fill="none" height="256" width="256" />
+      <polyline
+        fill="none"
+        points="86 58 128 16 170 58"
+        stroke="#000"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="8"
+      />
+      <line
+        fill="none"
+        stroke="#000"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="8"
+        x1="128"
+        x2="128"
+        y1="128"
+        y2="16"
+      />
+      <path
+        d="M176,96h24a8,8,0,0,1,8,8V208a8,8,0,0,1-8,8H56a8,8,0,0,1-8-8V104a8,8,0,0,1,8-8H80"
+        fill="none"
+        stroke="#000"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="8"
+      />
+    </svg>
+  );
+};
