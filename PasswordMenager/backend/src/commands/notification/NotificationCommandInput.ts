4c8148f1073a32efc826422dc246ece4df9266bd
hishira <okami0124@gmail.com>
2023-12-27 16:33:15 +0100

    Create delete command and handler

---
+++ PasswordMenager/backend/src/commands/notification/NotificationCommandInput.ts
@@ -0,0 +1,9 @@
+import { ObjectId } from 'mongoose';
+import { NotificationChannel } from 'src/schemas/Interfaces/notification.interface';
+
+export class NotificationCommandInput {
+  public readonly _id?: string | ObjectId;
+  public readonly notificationData?: string;
+  public readonly notificationChannel?: NotificationChannel;
+  public readonly active?: boolean;
+}
---
+++ PasswordMenager/backend/src/handlers/commands/notification/deleteNotificationHandler.ts
@@ -0,0 +1,16 @@
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { DeleteNotificationCommand } from 'src/commands/notification/DeleteNotificationCommand';
+import { BaseCommandHandler } from '../BaseCommandHandler';
+import { INotification } from 'src/schemas/Interfaces/notification.interface';
+
+@CommandHandler(DeleteNotificationCommand)
+export class DeleteNotificationCommandHandler
+  extends BaseCommandHandler<INotification>
+  implements ICommandHandler<DeleteNotificationCommand>
+{
+  execute(command: DeleteNotificationCommand): Promise<any> {
+    return this.repository.delete({
+      getOption: () => ({ _id: command.deleteNotificationInput._id }),
+    });
+  }
+}
