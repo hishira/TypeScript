13dfec829e180f0e88176bbaa6a44cf5075a7e64
hishira <okami0124@gmail.com>
2023-12-28 18:13:18 +0100

    Fix notification date view

--- PasswordMenager/backend/src/commands/notification/NotificationCommandInput.ts
+++ PasswordMenager/backend/src/commands/notification/NotificationCommandInput.ts
@@ -2,7 +2,7 @@ import { ObjectId } from 'mongoose';
 import { NotificationChannel } from 'src/schemas/Interfaces/notification.interface';
 
 export class NotificationCommandInput {
-  public readonly id?: string | ObjectId;
+  public readonly _id?: string | ObjectId;
   public readonly notificationData?: string;
   public readonly notificationChannel?: NotificationChannel;
   public readonly active?: boolean;
