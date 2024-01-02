175231650638473bdd0b034d15da0e038e433b63
hishira <okami0124@gmail.com>
2024-01-02 19:44:44 +0100

    Translation and translate option add

--- PasswordMenager/frontend/src/components/Translation/index.tsx
+++ PasswordMenager/frontend/src/components/Translation/index.tsx
@@ -1,9 +1,12 @@
 import { Translation as TT, useTranslation } from "react-i18next";
 
 export const TranslationFunction = (translationPath: string) => {
-    const {t} = useTranslation();
-    return t(translationPath);
-}
-export const Translation = (translationPath: string) => {
-  return <TT>{(t, i18n) => t(translationPath)}</TT>;
+  const { t } = useTranslation();
+  return t(translationPath);
+};
+export const Translation = (
+  translationPath: string,
+  options?: Record<any, any>
+) => {
+  return <TT>{(t, i18n) => t(translationPath, options)}</TT>;
 };
