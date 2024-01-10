584c9d3e05ceb52698b5535c8ede19b2f84c8c2b
hishira <okami0124@gmail.com>
2024-01-10 19:21:00 +0100

    Import request template for modal, and date fix

---
+++ PasswordMenager/frontend/src/components/AppBar/AccountContainer/AccountInfo/AccountInfoView/ImportRequestCardView/ImportRequestEntries/component.styled.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const ImportRequestEntriesModal = styled.div`
+  background-color: whitesmoke;
+  justify-self: center;
+  align-self: center;
+`;
+
+export const ImportRequestHeader = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 12px;
+`
