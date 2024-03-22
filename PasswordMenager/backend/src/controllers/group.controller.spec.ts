31d0392cf5c171512f5b6d0d4a4e1d388b778229
hishira <okami0124@gmail.com>
2024-03-22 22:56:55 +0100

    fix group controller

--- PasswordMenager/backend/src/controllers/group.controller.spec.ts
+++ PasswordMenager/backend/src/controllers/group.controller.spec.ts
@@ -1,31 +1,47 @@
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { EventEmitter2 } from '@nestjs/event-emitter';
 import { Test, TestingModule } from '@nestjs/testing';
-import { EntryRepository } from 'src/repository/entry.repository';
 import { GroupRepository } from 'src/repository/group.repository';
 import { Repository } from 'src/schemas/Interfaces/repository.interface';
-import { EntryService } from 'src/services/entry.service';
 import { GroupService } from 'src/services/group.service';
-import { TestUtils } from '../../test/utils/TestUtils';
-import { EntryMockModel } from '../../test/mock/EntryMock';
 import {
   CreateGroupDtoMock,
   GroupModelMock,
+  groupMock,
 } from '../../test/mock/GroupModelMock';
 import { UserRequestMock } from '../../test/mock/UserModelMock';
-import { GroupController } from './group.controller';
 import { TestDataUtils } from '../../test/utils/TestDataUtils';
+import { TestUtils } from '../../test/utils/TestUtils';
+import { GroupController } from './group.controller';
 
 describe('GroupController', () => {
   let groupController: GroupController;
   let groupService: GroupService;
-  let entryService: EntryService;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [GroupController],
       providers: [
         GroupService,
-        EntryService,
-        EntryRepository,
+        {
+          provide: EventEmitter2,
+          useValue: {
+            emit: jest.fn(),
+            emitAsync: jest.fn(),
+          },
+        },
+        {
+          provide: QueryBus,
+          useValue: {
+            execute: (...params) => Promise.resolve(groupMock()),
+          },
+        },
+        {
+          provide: CommandBus,
+          useValue: {
+            execute: (...params) => Promise.resolve(groupMock()),
+          },
+        },
         {
           provide: Repository,
           useClass: GroupRepository,
@@ -34,16 +50,11 @@ describe('GroupController', () => {
           provide: 'GROUP_MODEL',
           useValue: GroupModelMock,
         },
-        {
-          provide: 'ENTRY_MODEL',
-          useValue: EntryMockModel,
-        },
       ],
     }).compile();
 
     groupController = module.get<GroupController>(GroupController);
     groupService = module.get<GroupService>(GroupService);
-    entryService = module.get<EntryService>(EntryService);
   });
 
   beforeEach(() => jest.clearAllMocks());
@@ -107,7 +118,7 @@ describe('GroupController', () => {
     });
 
     it('Should use getbyuser from groupService', async () => {
-      const spy = jest.spyOn(entryService, 'deleteByGroup');
+      const spy = jest.spyOn(groupService, 'deleteGroup');
       await groupController.deleteByid(
         TestDataUtils.getRandomObjectIdAsString(),
       );
