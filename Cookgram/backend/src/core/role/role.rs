ebe99203fe251bbb2ad9e2f535cb3b26915a18d6
hishira <okami0124@gmail.com>
2024-04-10 23:12:21 +0200

    Add role mapping

--- Cookgram/backend/src/core/role/role.rs
+++ Cookgram/backend/src/core/role/role.rs
@@ -1,6 +1,9 @@
 use serde::{Deserialize, Serialize};
 
-pub trait Role {}
+pub struct Access {}
+pub trait Role {
+    fn has_access(access: Access) -> bool;
+}
 
 #[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
 pub struct UserRole {}
@@ -9,12 +12,36 @@ pub struct AdminRole {}
 #[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
 pub struct SuperAdminRole {}
 
-impl Role for UserRole {}
-impl Role for AdminRole {}
-impl Role for SuperAdminRole {}
+impl Role for UserRole {
+    fn has_access(_: Access) -> bool {
+        todo!()
+    }
+}
+impl Role for AdminRole {
+    fn has_access(_: Access) -> bool {
+        todo!()
+    }
+}
+impl Role for SuperAdminRole {
+    fn has_access(_: Access) -> bool {
+        todo!()
+    }
+}
 #[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
 pub enum Roles {
     User(UserRole),
     Admin(AdminRole),
     SuperAdmin(SuperAdminRole),
+}
+
+impl Roles {
+    pub fn user_role() -> Self {
+        Roles::User(UserRole {})
+    }
+    pub fn admin_role() -> Self {
+        Roles::Admin(AdminRole {})
+    }
+    pub fn super_admin_role() -> Self {
+        Roles::SuperAdmin(SuperAdminRole {})
+    }
 }
