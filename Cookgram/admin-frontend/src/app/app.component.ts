11f975bd623a900f307192ac6405887c46a4abad
hishira <okami0124@gmail.com>
2024-06-12 19:21:38 +0200

    Add more login to login page

--- Cookgram/admin-frontend/src/app/app.component.ts
+++ Cookgram/admin-frontend/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginPageComponent],
+  imports: [RouterOutlet, LoginPageComponent, ToastModule],
+  providers: [MessageService],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'admin-frontend';
--- Cookgram/admin-frontend/src/app/pages/login-page/login-page.component.ts
+++ Cookgram/admin-frontend/src/app/pages/login-page/login-page.component.ts
@@ -1,14 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CheckboxModule } from 'primeng/checkbox';
 import { RippleModule } from 'primeng/ripple';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
-
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { MessageService } from 'primeng/api';
+type LoginFormGroup = {
+  username: FormControl<string | null>;
+  password: FormControl<string | null>;
+};
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [CheckboxModule, RippleModule, ButtonModule, InputTextModule],
+  imports: [
+    CheckboxModule,
+    RippleModule,
+    ButtonModule,
+    InputTextModule,
+    ReactiveFormsModule,
+    NgIf,
+  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss',
 })
-export class LoginPageComponent {}
+export class LoginPageComponent {
+  readonly loginFormGroup: FormGroup<LoginFormGroup> =
+    new FormGroup<LoginFormGroup>({
+      username: new FormControl<string | null>('', [Validators.required]),
+      password: new FormControl<string | null>('', [Validators.required]),
+    });
+
+  constructor(private readonly messageService: MessageService) {}
+  signIn(): void {
+    if (!this.loginFormGroup.valid) {
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Error',
+        detail: 'Errors occurs in form',
+        key: 'br',
+        life: 2000,
+      });
+    }
+  }
+}
