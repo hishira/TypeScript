3655bb8b8a60317d94acf297e740c3efd0907837
hishira <okami0124@gmail.com>
2024-06-20 19:19:24 +0200

    Updated event

--- Cookgram/backend/src/core/event/userevent.rs
+++ Cookgram/backend/src/core/event/userevent.rs
@@ -1,9 +1,17 @@
-use super::event::{Event, EventType};
+use super::event::{Event, EventEntity, EventType};
 
 pub struct UserEvent {}
 
 impl UserEvent {
     pub fn create_event(id: uuid::Uuid) -> Event {
-        Event::new(None, Some(EventType::Create), id.clone(), true)
+        Event::new(None, Some(EventType::Create), EventEntity::User, id, true)
+    }
+
+    pub fn update_event(id: uuid::Uuid) -> Event {
+        Event::new(None, Some(EventType::Update), EventEntity::User, id, true)
+    }
+
+    pub fn delete_event(id: uuid::Uuid) -> Event {
+        Event::new(None, Some(EventType::Delete), EventEntity::User, id, true)
     }
 }
