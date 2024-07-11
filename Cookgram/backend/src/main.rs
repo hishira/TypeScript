607696d736f6890fe7f1a8384c6cfcb004e1a385
hishira <okami0124@gmail.com>
2024-07-12 00:19:00 +0200

    Fix reference

--- Cookgram/backend/src/main.rs
+++ Cookgram/backend/src/main.rs
@@ -1,12 +1,7 @@
-
 use api::{
-    applicationserver::applicationserver::ApplicationServer,
-    metrics::metricsserver::MetricsServer,
-
+    applicationserver::applicationserver::ApplicationServer, metrics::metricsserver::MetricsServer,
 };
-use axum::{
-    http::header::{AUTHORIZATION, CONTENT_TYPE},
-};
+use axum::http::header::{AUTHORIZATION, CONTENT_TYPE};
 use redis::{AsyncCommands, RedisError};
 
 mod api;
@@ -15,40 +10,8 @@ mod database;
 
 #[tokio::main]
 async fn main() {
-    match fetch_an_integer().await {
-        Ok(ok) => {
-            println!("OK");
-        }
-        Err(err) => {
-            tracing::error!("{}", err);
-        }
-    };
     let (_main_service, _metrics_service) = tokio::join!(
         ApplicationServer::start_main_app(),
         MetricsServer::start_metrics_server()
     );
 }
-
-// async fn fetch_an_integer() -> redis::RedisResult<()> {
-//     // connect to redis
-//     let redis_connection_string = ;
-//     let client = redis::Client::open(redis_connection_string).unwrap();
-//     let mut con = client.get_multiplexed_async_connection().await?;
-
-//     let _: () = con.set("key1", b"foo").await?;
-
-//     // redis::cmd("SET")
-//     //     .arg(&["key2", "bar"])
-//     //     .execute(&mut con)
-//     //     .await?;
-
-//     let result: Result<String, RedisError> = redis::cmd("MGET")
-//         .arg(&["key1"])
-//         .query_async(&mut con)
-//         .await;
-//     match result {
-//         Ok(ok) => println!("{}", ok),
-//         Err(error) => tracing::error!("{}", error),
-//     };
-//     Ok(())
-// }
