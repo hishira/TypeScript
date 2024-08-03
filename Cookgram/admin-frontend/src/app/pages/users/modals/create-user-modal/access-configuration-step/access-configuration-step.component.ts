fa64fe13b6f85df93ad4d0bbd43d1bc79928b1d4
hishira <okami0124@gmail.com>
2024-08-04 01:06:36 +0200

    Fix access configuration

--- Cookgram/admin-frontend/src/app/pages/users/modals/create-user-modal/access-configuration-step/access-configuration-step.component.ts
+++ Cookgram/admin-frontend/src/app/pages/users/modals/create-user-modal/access-configuration-step/access-configuration-step.component.ts
@@ -1,14 +1,22 @@
 import { AbstractStepDirective } from '../../../../../shared/directives/abstract-step.directive';
 import { Component } from '@angular/core';
 import { AccessConfigurationStepGroup } from '../create-user-model.types';
-import { DialogComponent } from "../../../../../shared/dialog/dialog.component";
-import { InputComponent } from "../../../../../shared/input/input.component";
+import { DialogComponent } from '../../../../../shared/dialog/dialog.component';
+import { InputComponent } from '../../../../../shared/input/input.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
   selector: 'app-access-configuration-step',
   templateUrl: './access-configuration-step.component.html',
   standalone: true,
-  imports: [DialogComponent, InputComponent, ReactiveFormsModule],
+  imports: [
+    DialogComponent,
+    InputComponent,
+    ReactiveFormsModule,
+    InputSwitchModule,
+    TooltipModule
+  ],
 })
 export class AccessConfigurationStep extends AbstractStepDirective<AccessConfigurationStepGroup> {}
