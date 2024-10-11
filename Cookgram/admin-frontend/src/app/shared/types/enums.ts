02eae7eacbbed6db72ec597b27b47453443c9dd8
hishira <okami0124@gmail.com>
2024-10-11 23:52:36 +0200

    Add enums

---
+++ Cookgram/admin-frontend/src/app/shared/types/enums.ts
@@ -0,0 +1,22 @@
+export enum Role {
+  User = 'User',
+  Admin = 'Admin',
+  SuperAdmin = 'SuperAdmin',
+  Employee = 'Employee',
+  Manager = 'Manager',
+  Director = 'Director',
+}
+
+export enum Gender {
+  Man = 'Man',
+  Woman = 'Woman',
+}
+
+export enum State {
+  Draft = 'Draft',
+  Active = 'Active',
+  Suspend = 'Suspend',
+  Frozen = 'Frozen',
+  Retired = 'Retired',
+  Deleted = 'Deleted',
+}
