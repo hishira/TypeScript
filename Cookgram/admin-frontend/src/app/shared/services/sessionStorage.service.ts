eed5f1cfa981be5ea349d2feab6c2b9275fdab01
hishira <okami0124@gmail.com>
2024-10-25 23:46:18 +0200

    Change session items  name to enum

--- Cookgram/admin-frontend/src/app/shared/services/sessionStorage.service.ts
+++ Cookgram/admin-frontend/src/app/shared/services/sessionStorage.service.ts
@@ -1,26 +1,32 @@
 import { Injectable } from '@angular/core';
 import { StorageItem } from '../types/shared';
+export enum SessionItemName {
+  Token = 'token',
+  Tokens  = 'tokens',
+  AccessToken = 'accessToken',
+  RefreshToken = 'refreshToken',
 
+}
 @Injectable({ providedIn: 'root' })
 export class SessionStorageService {
   private readonly session: Storage = sessionStorage;
 
-  getItem<T>(itemName: string): T {
+  getItem<T>(itemName: SessionItemName): T {
     const item = this.session.getItem(itemName);
     if (item === null)
       throw new Error(`Item ${itemName} not exists in session storage`);
     return JSON.parse(item);
   }
 
-  removeItem(itemName: string): void {
+  removeItem(itemName: SessionItemName): void {
     this.session.removeItem(itemName);
   }
 
-  removeItems(...itemName: string[]): void {
+  removeItems(...itemName: SessionItemName[]): void {
     itemName.forEach((name) => this.session.removeItem(name));
   }
 
-  setItem<T>(itemName: string, item: T): void {
+  setItem<T>(itemName: SessionItemName, item: T): void {
     this.session.setItem(itemName, JSON.stringify(item));
   }
 
