4a7e8606c40930669145f8a570d243daf9790125
hishira <okami0124@gmail.com>
2025-01-20 23:58:53 +0100

    Fixes

--- Cookgram/admin-frontend/src/app/shared/components/address/address.utils.ts
+++ Cookgram/admin-frontend/src/app/shared/components/address/address.utils.ts
@@ -2,14 +2,15 @@ import { AbstractControl, FormControl, FormGroup } from '@angular/forms';
 import { DefaultNonNullabeOption } from '../../consts/form.consts';
 import { AddressFields, AddressGroup, AddressRequiredMap } from './types';
 
+const EmptyString = '';
 export const EmptyAddressStep = (): FormGroup<AddressGroup> =>
   new FormGroup<AddressGroup>({
-    address: new FormControl<string>('', DefaultNonNullabeOption),
-    house: new FormControl<string>('', DefaultNonNullabeOption),
-    door: new FormControl<string>(''),
-    city: new FormControl<string>('', DefaultNonNullabeOption),
-    country: new FormControl<string>('', DefaultNonNullabeOption),
-    postalCode: new FormControl<string>('', DefaultNonNullabeOption),
+    address: new FormControl<string>(EmptyString, DefaultNonNullabeOption),
+    house: new FormControl<string>(EmptyString, DefaultNonNullabeOption),
+    door: new FormControl<string>(EmptyString),
+    city: new FormControl<string>(EmptyString, DefaultNonNullabeOption),
+    country: new FormControl<string>(EmptyString, DefaultNonNullabeOption),
+    postalCode: new FormControl<string>(EmptyString, DefaultNonNullabeOption),
   });
 
 export const EmptyAddressRequiredMap: AddressRequiredMap = {
