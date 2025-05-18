b8bbbe8010474e33030d7264c2c4117fa5b8e1e1
hishira <okami0124@gmail.com>
2025-05-18 02:38:31 +0200

    Fix

--- Cookgram/admin-frontend/src/app/pages/users/user-details/user-details.component.ts
+++ Cookgram/admin-frontend/src/app/pages/users/user-details/user-details.component.ts
@@ -3,16 +3,15 @@ import { Component, Signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { ChipModule } from 'primeng/chip';
 import { PanelModule } from 'primeng/panel';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
-import { ChipModule } from 'primeng/chip';
 import { SkeletonModule } from 'primeng/skeleton';
 import { UserApiSerivce } from '../../../../api/user.api';
 import { BaseComponent } from '../../../shared/components/base-component/base-component';
-import { UserDetails } from './types';
-import { ReadoOnlyComponent } from '../../../shared/components/readonly-only/readonly-only.component';
 import { UserAddressComponent } from './components/user-address/user-address.component';
 import { UserInfoComponent } from './components/user-info/user-info.component';
+import { UserDetails } from './types';
 
 @Component({
   selector: 'app-user-details',
@@ -25,7 +24,6 @@ import { UserInfoComponent } from './components/user-info/user-info.component';
     ProgressSpinnerModule,
     ChipModule,
     SkeletonModule,
-    ReadoOnlyComponent,
     UserAddressComponent,
     UserInfoComponent,
   ],
